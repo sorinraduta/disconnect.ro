642f537542a6b7bf1c768feb831fc9c67e49a2f1
Sorin Raduta <sorinradutaa@gmail.com>
2026-03-24 18:06:48 +0200

    env var

--- src/GoogleAnalytics.tsx
+++ src/GoogleAnalytics.tsx
@@ -26,7 +26,7 @@ const GoogleAnalytics = () => {
     return (
         <noscript>
             <iframe
-                src={`https://www.googletagmanager.com/ns.html?id=${GOOGLE_ANALYTICS_ID}`}
+                src={`https://www.googletagmanager.com/ns.html?id=${GOOGLE_TAG_MANAGER_ID}`}
                 height="0"
                 width="0"
                 style={{ display: "none", visibility: "hidden" }}
